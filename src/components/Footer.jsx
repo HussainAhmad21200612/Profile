200ac0c594e6b2d2f27127bce6921dea69cfe5b2
HUSSAIN AHMAD <hussain109164@gmail.com>
2025-09-06 22:01:44 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,11 @@ const Footer = () => {
   return (
       <Section className='min-h-fit flex flex-col justify-start items-start text-center py-16 px-5'>
           <p className='max-w-xlfont-light text-gray-500 mb-10 text-2xl'>
-              © 2022 <a href="https://github.com/hussainahmad21200612" target="_blank" rel="noopener noreferrer" className='hover:text-gray-900 dark:hover:text-cyan-500'>Hussain Ahmad</a> All Rights Reserved
+              © 2025 <a href="https://github.com/hussainahmad21200612" target="_blank" rel="noopener noreferrer" className='hover:text-gray-900 dark:hover:text-cyan-500'>Hussain Ahmad</a> All Rights Reserved
           </p>
     </Section>
   )
 }
+
 
 export default Footer
